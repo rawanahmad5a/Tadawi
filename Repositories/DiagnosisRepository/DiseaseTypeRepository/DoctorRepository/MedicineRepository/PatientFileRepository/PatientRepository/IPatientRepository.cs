b0c735af76d235fd4900937135db44820186efee
rawanahmad5a <rawanahmadaliabbas@gmail.com>
2025-12-28 01:26:42 +0300

    Add files via upload

---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/DoctorRepository/MedicineRepository/PatientFileRepository/PatientRepository/IPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Tadawi.Models;
+
+namespace Tadawi.Repositories.PatientRepository
+{
+    public interface IPatientRepository
+    {
+       public Task<List<Patient>> GetAsync();
+       public Task<Patient> GetByIdAsync(int id);
+       public Task<bool> AddAsync(Patient patient);
+       public Task<bool> UpdateAsync(Patient patient);
+       public  Task<bool> DeleteAsync(int id);
+        public Task<List<Patient>> PainetList();
+    }
+}
---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/DoctorRepository/MedicineRepository/PatientFileRepository/PatientRepository/PatientRepository.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tadawi.Data;
+using Tadawi.Models;
+using Tadawi.Repositories.PatientFileRepository;
+
+namespace Tadawi.Repositories.PatientRepository
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IPatientFileRepository _patientFileRepository;
+        public PatientRepository(ApplicationDbContext context, IPatientFileRepository patientFileRepository)
+        {
+            _context = context;
+            _patientFileRepository = patientFileRepository;
+        }
+
+        public async Task<List<Patient>> PainetList()
+        {
+            var patientList=await _context.Patients.Select(x => new Patient{ Id= x.Id, Name=x.Name}).ToListAsync();
+            return patientList;
+        }
+        public async Task<List<Patient>> GetAsync()
+        {
+            return await _context.Patients
+                .Include(p => p.PatientFile)
+                .ToListAsync();
+        }
+
+        public async Task<Patient> GetByIdAsync(int id)
+        {
+            return await _context.Patients
+                .Include(p => p.PatientFile)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> AddAsync(Patient patient)
+        {
+            if (patient == null)
+                return false;
+
+            int patientFileId = await _patientFileRepository.AddAsync(patient.Diagnosis);
+            if(patientFileId == 0)
+            {
+                return false;
+            }
+
+            patient.PatientFileId = patientFileId;
+            try
+            {
+                _context.Patients.Add(patient);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                await _patientFileRepository.DeleteAsync(patientFileId);
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Patient patient)
+        {
+            if (patient == null)
+                return false;
+
+            try
+            {
+                _context.Patients.Update(patient);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var entity = await _context.Patients.FindAsync(id);
+                if (entity == null)
+                    return false;
+
+                _context.Patients.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
