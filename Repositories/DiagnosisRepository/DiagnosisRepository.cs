0f5d97b730b2c3aa3e4975b56e1efbc76c489122
rawanahmad5a <rawanahmadaliabbas@gmail.com>
2025-12-28 01:21:55 +0300

    Add files via upload

---
+++ Repositories/DiagnosisRepository/DiagnosisRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tadawi.Data;
+using Tadawi.Models;
+
+namespace Tadawi.Repositories.DiagnosisRepository
+{
+    public class DiagnosisRepository : IDiagnosisRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiagnosisRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddAsync(Diagnosis diagnosis)
+        {
+            if (diagnosis == null) return false;
+            try
+            {
+                _context.Add(diagnosis);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public async Task<bool> UpdateAsync(Diagnosis diagnosis)
+        {
+            if (diagnosis == null) return false;
+            try
+            {
+                _context.Update(diagnosis);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var entity = await _context.Diagnoses.FindAsync(id);
+                if (entity == null) return false;
+
+                _context.Diagnoses.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public async Task<List<Diagnosis>> GetAsync()
+        {
+            return await _context.Diagnoses.ToListAsync();
+        }
+
+        public async Task<Diagnosis> GetByIdAsync(int id)
+        {
+            return await _context.Diagnoses.FirstOrDefaultAsync(d => d.Id == id);
+        }
+    }
+}
---
+++ Repositories/DiagnosisRepository/IDiagnosisRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tadawi.Models;
+
+namespace Tadawi.Repositories.DiagnosisRepository
+{
+    public interface IDiagnosisRepository
+    {
+      public Task<bool> AddAsync(Diagnosis diagnosis);
+      public Task<bool> UpdateAsync(Diagnosis diagnosis);
+      public Task<bool> DeleteAsync(int id);
+      public Task<List<Diagnosis>> GetAsync();
+      public Task<Diagnosis> GetByIdAsync(int id);
+    }
+}
