3c7805c57aedc7a64c0bf3e0b10e77d0c0dbc4e3
rawanahmad5a <rawanahmadaliabbas@gmail.com>
2025-12-28 01:08:44 +0300

    Add files via upload

---
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tadawi.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+            [EmailAddress(ErrorMessage = "بريد غير صالح")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "كلمة المرور مطلوبة")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var user = await _signInManager.UserManager.FindByEmailAsync(Input.Email);
+                    if (user != null)
+                    {
+                        if (await _signInManager.UserManager.IsInRoleAsync(user, "Admin"))
+                        {
+                            return LocalRedirect(Url.Content("~/Admin/Index"));
+                        }
+                        if (await _signInManager.UserManager.IsInRoleAsync(user, "Secretary"))
+                        {
+                            return LocalRedirect(Url.Content("~/Home/Index"));
+                        }
+                        if (await _signInManager.UserManager.IsInRoleAsync(user, "Doctor"))
+                        {
+                            return LocalRedirect(Url.Content("~/DoctorsDashboard/Index"));
+                        }
+                        if (await _signInManager.UserManager.IsInRoleAsync(user, "Pharmacist"))
+                        {
+                            return LocalRedirect(Url.Content("~/Pharmacist/Index"));
+                        }
+                    }
+
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "خطأ في البريد أو كلمة المرور.");
+                    return Page();
+                }
+            }
+            return Page();
+        }
+    }
+}
