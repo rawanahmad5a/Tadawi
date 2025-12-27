aa794c91929d9d21d4cd22340ba016000448473c
rawanahmad5a <rawanahmadaliabbas@gmail.com>
2025-12-28 01:22:45 +0300

    Add files via upload

---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/DiseaseTypeRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tadawi.Data;
+using Tadawi.Models;
+
+namespace Tadawi.Repositories.DiseaseTypeRepository
+{
+    public class DiseaseTypeRepository : IDiseaseTypeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiseaseTypeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddAsync(DiseaseType diseaseType)
+        {
+            if(diseaseType == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.Add(diseaseType);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex) { 
+                return false;
+            }
+
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var entity = await _context.DiseaseTypes.FindAsync(id);
+                if (entity == null)
+                    return false;
+
+                _context.DiseaseTypes.Remove(entity);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public async Task<List<DiseaseType>> GetAsync()
+        {
+            return await _context.DiseaseTypes.ToListAsync();
+        }
+
+        public async Task<DiseaseType> GetByIdAsync(int id)
+        {
+            return await _context.DiseaseTypes
+                .FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+
+        public async Task<bool> UpdateAsync(DiseaseType diseaseType)
+        {
+            if (diseaseType == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.Update(diseaseType);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/IDiseaseTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tadawi.Models;
+
+namespace Tadawi.Repositories.DiseaseTypeRepository
+{
+    public interface IDiseaseTypeRepository
+    {
+        public  Task<bool> AddAsync(DiseaseType diseaseType);
+        public  Task<bool> UpdateAsync(DiseaseType diseaseType);
+        public Task<bool> DeleteAsync(int id);
+        public Task<List<DiseaseType>> GetAsync();
+        public Task<DiseaseType> GetByIdAsync(int id);
+    }
+}
