9df12f67fce0563ff8df968e29c6bbfbcedc3c30
rawanahmad5a <rawanahmadaliabbas@gmail.com>
2025-12-28 01:30:55 +0300

    Add files via upload

---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/DoctorRepository/MedicineRepository/PatientFileRepository/PatientRepository/PeriodicDispenseRepository/PharmacyRepository/PrescriptionRepository/UserRepository/VisitRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tadawi.Repositories.UserRepository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<IdentityUser>> GetAllUsersAsync();
+        Task<IdentityResult> CreateUserAsync(IdentityUser user, string password, string role);
+        Task<bool> DeleteUserAsync(string userId);
+    }
+}
---
+++ Repositories/DiagnosisRepository/DiseaseTypeRepository/DoctorRepository/MedicineRepository/PatientFileRepository/PatientRepository/PeriodicDispenseRepository/PharmacyRepository/PrescriptionRepository/UserRepository/VisitRepository/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tadawi.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRepository(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IEnumerable<IdentityUser>> GetAllUsersAsync()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(IdentityUser user, string password, string role)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            if (result.Succeeded)
+            {
+                // التأكد من وجود الدور (الصلاحية) قبل إسنادها
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+                }
+                await _userManager.AddToRoleAsync(user, role);
+            }
+            return result;
+        }
+
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+                return result.Succeeded;
+            }
+            return false;
+        }
+    }
+}
